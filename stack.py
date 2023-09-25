1030e500eec0c9a789ee6f22aff734ed25020feb
bakaur21 <bakaur.ext@deloitte.com>
2023-09-25 16:41:36 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,17 +1,37 @@
-stack = []
- 
-stack.append('a')
-stack.append('b')
-stack.append('c')
- 
-print('Initial stack')
-print(stack)
- 
+class Stack:
+    def __init__(self):
+        self.items = []
 
-print('\nElements popped from stack:')
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
- 
-print('\nStack after elements are popped:')
-print(stack)
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Cannot pop from an empty stack")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Cannot peek into an empty stack")
+
+    def size(self):
+        return len(self.items)
+
+# Example usage:
+stack = Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print("Stack size:", stack.size())
+print("Top element:", stack.peek())
+
+while not stack.is_empty():
+    print("Popped:", stack.pop())
